810a21962cfd1721184cd99b90902fe2b08c9b14
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-03-05 16:16:31 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Kse.Algorithms.Samples;
+
+var generator = new MapGenerator(new MapGeneratorOptions()
+{
+    Height = 10,
+    Width = 10,
+});
+
+string[,] map = generator.Generate();
+new MapPrinter().Print(map);

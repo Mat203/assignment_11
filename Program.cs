863d72d63e58aa53957c89ecd8523ce1ec3d53b8
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-03-06 20:45:27 +0200

    Print function modified

--- Program.cs
+++ Program.cs
@@ -1,10 +1,49 @@
 ﻿using Kse.Algorithms.Samples;
 
+bool IsEqual(Point a, Point b)
+{
+    return a.Column == b.Column && a.Row == b.Row;
+}
+
+void PrintMap(string[,] map, List<Point> path)
+{
+    Point start = path[0];
+    Point end = path[^1];
+
+    foreach (Point p in path)
+    {
+        if (IsEqual(p, start))
+        {
+            map[p.Column, p.Row] = "A";
+        }
+        else if (IsEqual(p, end))
+        {
+            map[p.Column, p.Row] = "B";
+        }
+        else
+        {
+            map[p.Column, p.Row] = ".";
+        }
+    }
+    new MapPrinter().Print(map);
+}
+
 var generator = new MapGenerator(new MapGeneratorOptions()
 {
     Height = 10,
     Width = 10,
+    Seed = 1,
 });
 
 string[,] map = generator.Generate();
-new MapPrinter().Print(map);
+
+List<Point> path = new List<Point>(new Point[]
+{
+    new Point(0,0),
+    new Point(0,1),
+    new Point(0,2),
+    new Point(0,3)
+});
+
+PrintMap(map, path);
+

7d1b008cf218ec5e26b9d508a037345fa3dcf2dc
Anastasiasushko <anastasiasusko2109@gmail.com>
2023-03-09 19:23:37 +0200

    пошук в ширину

--- Program.cs
+++ Program.cs
@@ -28,11 +28,84 @@ void PrintMap(string[,] map, List<Point> path)
     new MapPrinter().Print(map);
 }
 
+bool IsWall(string c)
+{
+    return c == "█";
+}
+List<Point> Neighbours(string[,] map, Point p)
+{
+    List<Point> result = new List<Point>();
+
+    int x = p.Column;
+    int y = p.Row;
+
+    if (y++ < map.GetLength(1) && y++ >= 0 && x < map.GetLength(0) && x >= 0 && !IsWall(map[x, y++]))
+    {
+        result.Add(new Point(x,y++));
+    }
+    if (y-- < map.GetLength(1) && y-- >= 0 && x < map.GetLength(0) && x >= 0 && !IsWall(map[x, y++]))
+    {
+        result.Add(new Point(x,y--));
+        
+    }if (y < map.GetLength(1) && y >= 0 && x++ < map.GetLength(0) && x++ >= 0 && !IsWall(map[x, y++]))
+    {
+        result.Add(new Point(x++,y));
+    }if (y < map.GetLength(0) && y >= 0 && x-- < map.GetLength(0) && x-- >= 0 && !IsWall(map[x, y++]))
+    {
+        result.Add(new Point(x--,y));
+    }
+
+    return result;
+}
+
+List<Point> SearchBFS(string[,] map,Point start, Point end)
+{
+    var frontier = new Queue<Point>();
+    Dictionary<Point, Point?> cameFrom = new Dictionary<Point, Point?>();
+    
+    cameFrom.Add(start, null);
+    frontier.Enqueue(start);
+
+    while (frontier.Count > 0)
+    {
+        var cur = frontier.Dequeue();
+        
+        if (IsEqual(cur, end))
+        {
+            break;
+        }
+
+        foreach (Point neighbour in Neighbours(map, cur))
+        {
+            if (!cameFrom.TryGetValue(neighbour, out _))
+            {
+                cameFrom.Add(neighbour, cur);
+                frontier.Enqueue(neighbour);
+            } 
+        }
+    }
+
+    List<Point> path = new List<Point>();
+
+    Point? current = end;
+    while (IsEqual(current.Value, start))
+    {
+        path.Add(current.Value);
+        cameFrom.TryGetValue(current.Value, out current);
+    }
+    path.Add(start);
+
+    path.Reverse();
+    return path;
+
+}
+
+;
+
 var generator = new MapGenerator(new MapGeneratorOptions()
 {
     Height = 10,
-    Width = 10,
-    Seed = 1,
+    Width = 15,
 });
 
 string[,] map = generator.Generate();
@@ -47,3 +120,6 @@ List<Point> path = new List<Point>(new Point[]
 
 PrintMap(map, path);
 
+
+
+pri
